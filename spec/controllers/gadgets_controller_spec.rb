9e3c9593adae649eff0bb5640f2341e97710b527
ellmo <ellmunzai@gmail.com>
2013-11-19 21:10:44 +0100

    Gadgets#show spec

--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 
 describe GadgetsController do
   before { 3.times { FactoryGirl.create :gadget }}
+  let(:gadget) { Gadget.last }
+
   context 'GET index' do
     before { get :index }
+    it 'is success' do
+      expect(response).to be_success
+    end
     it 'assigns properly' do
       expect(assigns :gadgets).to eq Gadget.all
+    end
+  end
+
+  context 'GET show' do
+    before { get :show, id: gadget.id }
+    it 'is success' do
+      expect(response).to be_success
+    end
+    it 'assigns properly' do
+      expect(assigns :gadget).to eq gadget
     end
   end
 end
