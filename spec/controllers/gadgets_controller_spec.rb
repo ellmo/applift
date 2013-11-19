aa29d2176994efbcdf9fdb48561d257d9c552bcb
ellmo <ellmunzai@gmail.com>
2013-11-19 21:04:43 +0100

    Gadgets#index spec

--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe GadgetsController do
-
+  before { 3.times { FactoryGirl.create :gadget }}
+  context 'GET index' do
+    before { get :index }
+    it 'assigns properly' do
+      expect(assigns :gadgets).to eq Gadget.all
+    end
+  end
 end
