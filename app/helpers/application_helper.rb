6ab6034c1b1d668c0f0b4b3859b044bbfbc58089
ellmo <ellmunzai@gmail.com>
2013-11-19 19:21:36 +0100

    added devise links to layout view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def user_login_span_tag(user = nil)
+    if user
+      str = <<-HTML
+      <div>
+        <span>#{user.login} |<span>
+        <span>#{link_to("Log out", destroy_user_session_path, :method => :delete)}</span>
+      </div>
+      HTML
+    else
+      str = <<-HTML
+      <div>
+        <span>#{link_to("Log in", new_user_session_path)}<span> |
+        <span>#{link_to("Register", new_user_registration_path)}</span>
+      </div>
+      HTML
+    end
+    raw str
+  end
+
 end
