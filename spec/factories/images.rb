b16016dbb8c5506a8fa07c418fea8268f1931765
ellmo <ellmunzai@gmail.com>
2013-11-19 20:20:17 +0100

    improved image factory

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,7 +1,19 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+include ActionDispatch::TestProcess
 
 FactoryGirl.define do
   factory :image do
-    gadget nil
+    gadget
+    image_file { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'ellmo.png')) }
+
+    trait :png do
+      image_file { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'ellmo.png')) }
+    end
+
+    trait :jpg do
+      image_file { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'ellmo.jpg')) }
+    end
+
+    factory :image_png, traits: [:png]
+    factory :image_jpg, traits: [:jpg]
   end
 end
